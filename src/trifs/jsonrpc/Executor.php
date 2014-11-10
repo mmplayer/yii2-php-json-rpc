5031f1358ac3fbc2843cf6946f218c07332c58d8
michalliu <michalliu@foxmail.com>
2014-11-10 17:09:01 +0800

    refractor

--- src/trifs/jsonrpc/Executor.php
+++ src/trifs/jsonrpc/Executor.php
@@ -1,32 +1,53 @@
-<?
+<?php
 namespace trifs\jsonrpc;
 
 use Yii;
 use trifs\jsonrpc\Server;
+use yii\base\Object;
+use yii\base\Exception;
 
-class Executor {
-    private static function invoke($type, $params, $method, $methodParams) {
+class Executor extends Object {
+
+    private static $TAG="JSONRPC";
+
+    // under which namespace the handlers will execute
+    private $_handlerNamespace;
+
+    // setter
+    public function setHandlerNamespace($ns){
+        $this->_handlerNamespace = $ns;
+    }
+
+    /**
+     * @param $type string|array|callable $type the object type.
+     * @param $params array $params the constructor parameters
+     * @param $method string to call
+     * @param $methodParams array params passed to method
+     * @return mixed
+     * @throws \yii\base\InvalidConfigException
+     * @throws
+     */
+    private function invoke($type, $params, $method, array $methodParams) {
         if(is_null($params)) {
             $params = [];
         }
+        // create instance from configuration
         $component = Yii::createObject($type, $params);
         if (method_exists($component, $method) && is_callable([$component, $method])) {
             $reflectionMethod = new \ReflectionMethod($component, $method);
             $requiredParamsCount = $reflectionMethod->getNumberOfRequiredParameters();
             $methodParamsCount=count($methodParams);
             if ($methodParamsCount < $requiredParamsCount) {
-                return call_user_func("\\trifs\\jsonrpc\\Message::invalidParams");
+                throw new Exception(Server::MESSAGE_ERROR_INVALID_PARAMS,Server::ERROR_INVALID_PARAMS);
             } else {
                 return call_user_func_array([$component, $method], $methodParams);
             }
         } else {
-            return call_user_func("\\trifs\\jsonrpc\\Message::methodNotFound");
+            throw new Exception(Server::MESSAGE_ERROR_METHOD_NOT_FOUND,Server::ERROR_METHOD_NOT_FOUND);
         }
     }
 
-    public static $handlerNamespace;
-
-    public static function invoker($method, array $params=[]) {
+    public function invoker($method, array $params=[]) {
         $methodGroup = explode(".", $method);
         if (count($methodGroup) > 1) {
             $methodName = array_pop($methodGroup);
@@ -34,22 +55,22 @@ class Executor {
                 array_map(["yii\\helpers\\BaseInflector","camelize"],
                     $methodGroup)
             );
-            $klass=self::$handlerNamespace . "\\handlers\\" . $klassPath;
-            Yii::info("JSONRPC\n class => ". $klass . "\n method => " . $methodName . "\n params => " . implode(",", $params));
-            return self::invoke([
+            $klass=$this->_handlerNamespace . "\\" . $klassPath;
+            Yii::info(self::$TAG . "\n class => ". $klass . "\n method => " . $methodName . "\n params => " . implode(",", $params));
+            return $this->invoke([
                 "class" => $klass
             ], null, $methodName, $params);
         } else {
-            $klass=self::$handlerNamespace . "\\handlers\\DefaultHandler";
-            Yii::info("JSONRPC\n class => ". $klass . "\n method => " . $method . "\n params => " . implode(",", $params));
-            return self::invoke([
+            $klass=$this->_handlerNamespace . "\\". "DefaultHandler";
+            Yii::info(self::$TAG . "\n class => ". $klass . "\n method => " . $method . "\n params => " . implode(",", $params));
+            return $this->invoke([
                 "class" => $klass
             ], null, $method, $params);
         }
     }
 
-    public static function execute($input) {
-       $server = new Server($input, ["trifs\\jsonrpc\\Executor","invoker"]);
-       return $server->run();
+    public function execute($input) {
+        $server = new Server($input, [$this,"invoker"]);
+        return $server->run();
     }
 }
