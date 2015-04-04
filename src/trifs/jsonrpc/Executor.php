c0ccf7c8eced074e9a27446d67271c3178b6a73f
michalliu <michalliu@foxmail.com>
2015-04-04 15:12:50 +0800

    1. 屏蔽掉override的方法和父类的方法 2. 使用对象本身的class判断，而不是传入值，避免类大小写带来的方法找不到问题,用户的传入只决定yii createObject是否成功，不影响方法是否存在的判断

--- src/trifs/jsonrpc/Executor.php
+++ src/trifs/jsonrpc/Executor.php
@@ -4,6 +4,7 @@ namespace trifs\jsonrpc;
 use Yii;
 use trifs\jsonrpc\Server;
 use trifs\jsonrpc\JSONRPCError;
+use yii\base\Exception;
 use yii\base\Object;
 
 class Executor extends Object {
@@ -45,7 +46,16 @@ class Executor extends Object {
         $component = Yii::createObject($type, $params);
         if (method_exists($component, $method) && is_callable([$component, $method])) {
             $reflectionMethod = new \ReflectionMethod($component, $method);
-            if ($reflectionMethod->isPublic()) {
+            try {
+                $proto = $reflectionMethod->getPrototype();
+            } catch (\ReflectionException $e){ // 如果不是override的方法，getPrototype会抛异常
+                $proto = null; // 父类不存在该方法
+            }
+            //$protoKlass = $proto->getDeclaringClass()->getName();
+            if ($reflectionMethod->isPublic() && // 公开的方法
+                !$proto && // 非Override的父类，原型（父类）中此方法不存在
+                (get_class($component) == $reflectionMethod->getDeclaringClass()->getName()) // 非是继承来的父类方法（DeclaringClass等于自身）
+            ) {
                 $requiredParamsCount = $reflectionMethod->getNumberOfRequiredParameters();
                 $methodParamsCount=count($methodParams);
                 if ($methodParamsCount < $requiredParamsCount) {
@@ -70,22 +80,30 @@ class Executor extends Object {
                     $methodGroup)
             );
             $klass=$this->_handlerNamespace . "\\" . $klassPath;
-            $bundleProperty = new \ReflectionProperty($klass,"bundle");
+            try {
+                $bundleProperty = new \ReflectionProperty($klass, "bundle");
+            } catch(\ReflectionException $e) {
+                $bundleProperty = null;
+            }
             $config["class"] = $klass;
             // We only pass bundles from controller only if there is a field named bundle and it's public.
             // WARNING: You should NOT use getter provide by yii framework in JSONRPC handlers.
             // because it must be public and accessible by any JSONRPC call. eg. if public getBundle() exists in handlers
             // anyone can call it then your bundle info is leaked.
-            if ($bundleProperty->isPublic()){
+            if ($bundleProperty && $bundleProperty->isPublic()){
                 $config["bundle"] =  $this->bundle;
             }
             Yii::info(self::$TAG . "\n class => ". $klass . "\n method => " . $methodName . "\n params => " . implode(",", $params));
             return $this->invoke($config, null, $methodName, $params);
         } else {
             $klass=$this->_handlerNamespace . "\\". "DefaultHandler";
-            $bundleProperty = new \ReflectionProperty($klass,"bundle");
+            try {
+                $bundleProperty = new \ReflectionProperty($klass, "bundle");
+            } catch (\ReflectionException $e){
+                $bundleProperty = null;
+            }
             $config["class"] = $klass;
-            if ($bundleProperty->isPublic()){
+            if ($bundleProperty && $bundleProperty->isPublic()){
                 $config["bundle"] =  $this->bundle;
             }
             Yii::info(self::$TAG . "\n class => ". $klass . "\n method => " . $method . "\n params => " . implode(",", $params));
